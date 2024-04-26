64b29611dc34f29c99ce39cd1c4403638df7b76c
adlansubejo <m.faliq.adlan@roketin.com>
2024-04-26 20:58:49 +0700

    start create corr

---
+++ main.py
@@ -0,0 +1,4 @@
+#
+
+# genereta code to do explatory data analyst to analyze correlation and causation from file with path, dataSheet\weatherAUS.csv with the column is Date,Location,MinTemp,MaxTemp,Rainfall,Evaporation,Sunshine,WindGustDir,WindGustSpeed,WindDir9am,WindDir3pm,WindSpeed9am,WindSpeed3pm,Humidity9am,Humidity3pm,Pressure9am,Pressure3pm,Cloud9am,Cloud3pm,Temp9am,Temp3pm,RainToday,RainTomorrow
+
