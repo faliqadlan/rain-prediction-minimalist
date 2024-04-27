63511bf5dbcd5ec41f94420bb64eef0b081b07ff
adlansubejo <m.faliq.adlan@roketin.com>
2024-04-27 15:41:38 +0700

    delete to make lighter

--- main.py
+++
@@ -1,4 +0,0 @@
-#
-
-# genereta code to do explatory data analyst to analyze correlation and causation from file with path, dataSheet\weatherAUS.csv with the column is Date,Location,MinTemp,MaxTemp,Rainfall,Evaporation,Sunshine,WindGustDir,WindGustSpeed,WindDir9am,WindDir3pm,WindSpeed9am,WindSpeed3pm,Humidity9am,Humidity3pm,Pressure9am,Pressure3pm,Cloud9am,Cloud3pm,Temp9am,Temp3pm,RainToday,RainTomorrow
-
